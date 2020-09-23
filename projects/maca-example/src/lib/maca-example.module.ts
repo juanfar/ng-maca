567b5b952158a25060fb3776901694fc5789f7bd
Juan Fajardo <juanfar.group@gmail.com>
2020-09-23 18:30:17 +0200

    Creacion del proyecto y el componente example

---
+++ projects/maca-example/src/lib/maca-example.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MacaExampleComponent } from './maca-example.component';
+
+
+
+@NgModule({
+  declarations: [MacaExampleComponent],
+  imports: [
+  ],
+  exports: [MacaExampleComponent]
+})
+export class MacaExampleModule { }
